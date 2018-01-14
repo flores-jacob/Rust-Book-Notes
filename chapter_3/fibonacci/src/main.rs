34c98c144e81774860ddc684c16d10d9c2b1fce5
phwebmachine <phwebmachine@gmail.com>
2018-01-14 21:21:05 +0800

    feat: Implement fibonacci

--- chapter_3/fibonacci/src/main.rs
+++ chapter_3/fibonacci/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter n for the fibonacci sequence");
+
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Failed to read line");
+
+    let n: u32 = n.trim().parse().unwrap();
+
+//    let n: u32 = match n.trim().parse() {
+//        Ok(num) => num,
+//        Err(_) => continue,
+//    };
+    let mut prevoius2_num: u32 = 0;
+    let mut previous_num: u32 = 1;
+    let mut current_num: u32 = 0;
+
+    if n > 0 {
+        for _ in 0..n -2 {
+            current_num = previous_num + prevoius2_num;
+            prevoius2_num = previous_num;
+            previous_num = current_num;
+        }
+        println!("The fibonacci number at position {} is {}", n, current_num);
+    } else {
+        println!("The fibonacci number at position 1 is 0");
+    }
 }
